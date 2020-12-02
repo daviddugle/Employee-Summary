60b81eaf6f61b68bfb8f80932f30ebec32106f63
David Dugle <david.dugleii@yahoo.com>
2020-12-01 21:18:58 -0500

    touch ups, added an array that the data would push into

--- app.js
+++ app.js
@@ -14,16 +14,6 @@ const render = require("./lib/htmlRenderer");
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
 // HINT: each employee type (manager, engineer, or intern) has slightly different
 // information; write your code to ask different questions via inquirer depending on
 // employee type.
@@ -33,6 +23,8 @@ const render = require("./lib/htmlRenderer");
 // for further information. Be sure to test out each class and verify it generates an
 // object with the correct structure and methods. This structure will be crucial in order
 // for the provided `render` function to work! ```
+
+const teamMake = [];
 
 const whatNext = () => {
     inquirer.prompt([
@@ -83,7 +75,8 @@ const buildManager = () => {
     ])
         .then(({ name, id, email, officeNumber }) => {
             const newMgr = new Manager(name, id, email, officeNumber)
-            console.log(newMgr)
+            console.log(newMgr);
+            teamMake.push(newMgr);
             whatNext();
         })
 }
@@ -115,10 +108,11 @@ const buildEngineer = () => {
     ])
         .then(({ name, id, email, github }) => {
             const newEngineer = new Engineer(name, id, email, github)
-            console.log(newEngineer)
+            console.log(newEngineer);
+            teamMake.push(newEngineer);
             whatNext();
         })
-}
+};
 const buildIntern = () => {
     inquirer.prompt([
         {
@@ -144,8 +138,23 @@ const buildIntern = () => {
         },
     ])
         .then(({ name, id, email, school }) => {
-            const newIntern = new Engineer(name, id, email, school)
-            console.log(newIntern)
+            const newIntern = new Intern(name, id, email, school)
+            console.log(newIntern);
+            teamMake.push(newIntern);
             whatNext();
         })
+};
+
+// After the user has input all employees desired, call the `render` function (required
+// above) and pass in an array containing all employee objects; the `render` function will
+// generate and return a block of HTML including templated divs for each employee!
+
+// After you have your html, you're now ready to create an HTML file using the HTML
+// returned from the `render` function. Now write it to a file named `team.html` in the
+// `output` folder. You can use the variable `outputPath` above target this location.
+// Hint: you may need to check if the `output` folder exists and create it if it
+// does not.
+const buildTeam = () =>{
+
+
 }
